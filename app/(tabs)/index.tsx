0d4d4918f2e9e38b51f9d371144711c5b58e6480
Narvasiddhartha <narvasiddhartha@gmail.com>
2025-03-12 00:43:35 +0530

    first commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { StyleSheet } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
 import CaloriesTracker from '@/components/CaloriesTracker';
 
-export default function HomeScreen() {
+export default function TabHomeScreen() {
   return (
     <ThemedView style={styles.container}>
       <CaloriesTracker />
@@ -13,5 +13,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#1E1E2E',
   },
 });
